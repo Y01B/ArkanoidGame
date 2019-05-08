f4d3e4278e2b9c142250c19fa570524c3e33a271
AaJanus <a2janus@wp.pl>
2019-05-08 23:28:02 +0200

    az do programowania kulki

--- ArkanoidGame/Assets/LoseCollider.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LoseCollider : MonoBehaviour { }
-private void OnTriggerEnter2D(Collider2Dcollision) { }
