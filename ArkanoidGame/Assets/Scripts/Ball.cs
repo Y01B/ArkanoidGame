d43bbe418a0f6cecf46a21857ce49d2e78b60e4a
AaJanus <a2janus@wp.pl>
2019-05-09 09:44:54 +0200

    Ball sticks to the paddle

--- ArkanoidGame/Assets/Scripts/Ball.cs
+++ ArkanoidGame/Assets/Scripts/Ball.cs
@@ -6,7 +6,10 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
 
+    public Transform Paddle;
     Vector2 paddleToBallVector;
+    public bool inPlay;
+    
 
     // Start is called before the first frame update
     private void Start()
@@ -19,6 +22,11 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
    private void Update()
     {
+        if(!inPlay)
+        {
+            transform.position = Paddle.position;
+        }
+
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
     }
