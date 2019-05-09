63da6ec14dea67e32ba1bb1da8a704d6a8d4f5a6
AaJanus <a2janus@wp.pl>
2019-05-09 08:41:38 +0200

    Load Level changes

--- ArkanoidGame/Assets/Scripts/LoseCollider.cs
+++ ArkanoidGame/Assets/Scripts/LoseCollider.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoseCollider : MonoBehaviour {
-    private void OnTriggerEnter2D(Collider2D collision)
+
+      private void OnTriggerEnter2D(Collider2D collision)
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
