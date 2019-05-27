7dd9dff8016b8249a0e0842a8c8a315f52f7fc3e
AaJanus <a2janus@wp.pl>
2019-05-27 14:59:51 +0200

    ball bounces of the paddle and the edges of the screen + mixed instructions

--- ArkanoidGame/Assets/Scripts/Ball.cs
+++ ArkanoidGame/Assets/Scripts/Ball.cs
@@ -1,33 +1,67 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
+    [SerializeField] Rigidbody2D rb;
 
-    public Transform Paddle;
+
+    public Transform Paddle; //stores reference to the paddle
     Vector2 paddleToBallVector;
     public bool inPlay;
-    
 
     // Start is called before the first frame update
     private void Start()
     {
-        paddleToBallVector = transform.position - paddle1.transform.position;
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.simulated = false;
+
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-   private void Update()
+    private void Update()
     {
-        if(!inPlay)
+        if (!inPlay)
         {
             transform.position = Paddle.position;
+
         }
 
-        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
-        transform.position = paddlePos + paddleToBallVector;
+        LaunchToPaddle();
+
+    }
+
+    private void LaunchToPaddle()
+    {
+        if (Input.GetMouseButtonDown(0) && !inPlay)
+        {
+            inPlay = true;
+            rb.simulated = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            rb.AddForce(Vector2.up * 500);
+        }
+           
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Bottom"))
+        {
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+
+        }
+    
+    
+        
     }
 }
+
+
+  
