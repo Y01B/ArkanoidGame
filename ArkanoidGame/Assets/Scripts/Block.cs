fa823ae082c458706b8cd3b41ef41906ab02781a
AaJanus <a2janus@wp.pl>
2019-05-29 23:02:51 +0200

    Can now destroy blocks!

---
+++ ArkanoidGame/Assets/Scripts/Block.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public int hitsToKill;
+    public int points;
+    private int numberOfHits;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Ball")
+        {
+            numberOfHits++;
+
+            if (numberOfHits == hitsToKill)
+            {
+                // destroy the object
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+}
