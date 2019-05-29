dd239c140414e0b935337a407872de26e84d8c76
AaJanus <a2janus@wp.pl>
2019-05-30 01:14:15 +0200

    Added custom background

--- ArkanoidGame/Assets/Scripts/Block.cs
+++ ArkanoidGame/Assets/Scripts/Block.cs
@@ -7,20 +7,38 @@ public class Block : MonoBehaviour
     public int hitsToKill;
     public int points;
     private int numberOfHits;
+    private SpriteRenderer myspriteRenderer;
+     
+    string Block_Square;
+
+    private void Start()
+    {
+        myspriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
 
         if (collision.gameObject.tag == "Ball")
         {
+
             numberOfHits++;
 
+            if (numberOfHits == hitsToKill +1)
+            {
+                
+            }
             if (numberOfHits == hitsToKill)
             {
                 // destroy the object
                 Destroy(this.gameObject);
             }
+            Debug.Log(collision.gameObject.name);
         }
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
 
+    }
 }
